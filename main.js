fe563f42606e8a1b9d0253df7c697e71f247240d
wi <wylite@autotainer>
2021-06-30 13:15:41 +0000

    a

---
+++ main.js
@@ -0,0 +1,25 @@
+const discord = require('discord.js')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config();
+const Client = require('./lib/client.js')
+
+const client = new Client({
+	ws: {
+		intents: discord.Intents.ALL,
+	},
+	partials: [
+		"MESSAGE",
+		"REACTION",
+		"CHANNEL",
+	],
+}, {
+	commandsDir: 'cmd',
+	listenersDir: 'events',
+});
+
+client.login(process.env.DISCORD_TOKEN);
+mongoose.connect(process.env.MONGODB_TOKEN, {useNewUrlParser: true, useUnifiedTopology: true});
+
+client.loadAll();
